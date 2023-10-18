8b5e34fe5368246986c49de24009ea9e7a70b719
joseph2462 <joseph2462@hansei.ac.kr>
2023-10-18 11:04:01 +0900

    웹 백엔드
    웹 백엔드 코드 및 테스트 코드

--- Framework/framework/Express.js
+++ Framework/framework/Express.js
@@ -1,30 +1,59 @@
 const express = require('express');
-const openai = require('openai');
-require('dotenv').config(); // .env 파일 로드
-
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
-app.use(express.json());
+// OpenAI API 키 설정
+const OPENAI_API_KEY = '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';
 
-app.post('/generate-text', async (req, res) => {
-  const { prompt } = req.body;
+app.use(bodyParser.json());
 
+// 클라이언트에서 POST 요청을 받는 엔드포인트
+app.post('/ask-gpt', async (req, res) => {
+  const { question } = req.body;
+
+  // GPT-3에 질문 보내기
   try {
-    const response = await openai.Completion.create({
-      engine: "text-davinci-002",
-      prompt: prompt,
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+      prompt: question,
       max_tokens: 50,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
+      }
     });
 
-    const generatedText = response.choices[0].text;
-    res.json({ text: generatedText });
+    const answer = response.data.choices[0].text;
+    res.json({ answer });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
+// 서버 시작
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// 클라이언트 코드 (예를 들어, 웹 브라우저에서 실행)
+const fetch = require('node-fetch'); // Node.js에서 fetch 사용
+
+const question = 'What is the capital of France?'; // 질문 내용
+
+fetch('http://localhost:3000/ask-gpt', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ question }), // 질문을 서버에 보냄
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('GPT-3 응답:', data.answer); // GPT-3의 응답을 출력
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });
+
 
