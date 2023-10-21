af80b7a6e3f0a40b87c32f3ee243b23410021f4f
joseph2462 <joseph2462@hansei.ac.kr>
2023-10-21 15:04:48 +0900

    프론트 엔드 병합과 백엔드 완성 및 테스트
    프론트 엔드와의 테스트를 위하여 미리 병합하고 백엔드 완성과 테스트 코드 작업 (이후 프론트엔드와 데이터 베이스 병합을 할 예정)

--- Framework/framework/client.js
+++ Framework/framework/client.js
@@ -1,18 +1,30 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
-const question = 'What is the capital of France?';
+const OPENAI_API_KEY = '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'; // 여기에 올바른 API 키를 넣으세요
 
-fetch('http://localhost:3000/ask-gpt', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ question }),
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log('GPT-3 응답:', data.answer);
-  })
-  .catch(error => {
-    console.error('An error occurred:', error);
-  });
+async function askGPT(question) {
+  try {
+    const requestBody = {
+      prompt: question,
+      max_tokens: 50, // 답변의 최대 토큰 수
+      temperature: 0.7, // 다양성을 조절하는 옵션 (0.2부터 1.0까지 설정 가능)
+    };
+
+    const headers = {
+      'Authorization': `Bearer ${OPENAI_API_KEY}`,
+      'Content-Type': 'application/json',
+    };
+
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', requestBody, { headers });
+
+    const answer = response.data.choices[0].text;
+    return answer;
+  } catch (error) {
+    console.error('An error occurred while interacting with GPT-3:', error);
+    throw error; // 다시 에러를 던져서 호출하는 쪽에서 처리할 수 있도록 함
+  }
+}
+
+module.exports = askGPT;
+
+
