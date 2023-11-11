56173be14ee3ef73b669ffdafc3abc1f95ca8b09
joseph2462 <joseph2462@hansei.ac.kr>
2023-11-11 13:45:29 +0900

    Revert "백엔드 준완성"
    This reverts commit 33250cda0d7f7e840783cb999fdf6b6ccf682d93.

--- Framework/framework/server.js
+++ Framework/framework/server.js
@@ -1,45 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const mysql = require('mysql2'); // MySQL 모듈 추가
 const app = express();
 
 app.use(bodyParser.json());
-
-// MySQL 연결 정보
-const db = mysql.createConnection({
-    host: 'localhost', // MySQL 호스트
-    user: 'root', // MySQL 사용자 이름
-    password: 'yourpassword', // MySQL 비밀번호
-    database: 'yourdatabase' // 데이터베이스 이름
-});
 
 // GPT-3 API 키 설정
 const OPENAI_API_KEY = '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';
 
 // 프론트엔드에서 POST 요청을 받아 처리
-app.post('/api/gpt', async (req, res) => {
-    const { question } = req.body;
+app.post('/ask-gpt', async (req, res) => {
+  const { question } = req.body;
 
-    try {
-        const answer = await askGPT(question);
+  try {
+    const answer = await askGPT(question); // 클라이언트 코드를 통해 GPT-3 API 호출
 
-        // 백엔드에서 MySQL 데이터베이스에 데이터 삽입
-        db.query('INSERT INTO questions (question, answer) VALUES (?, ?)', [question, answer], (err, result) => {
-            if (err) {
-                console.error('MySQL 오류:', err);
-            } else {
-                console.log('데이터 삽입 성공');
-            }
-        });
-
-        res.json({ answer });
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred' });
-    }
+    res.json({ answer });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-const port = 5000;
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
+// askGPT 함수 정의
+async function askGPT(question) {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+      prompt: question,
+      max_tokens: 50,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
+      }
+    });
+
+    const answer = response.data.choices[0].text;
+    return answer;
+  } catch (error) {
+    throw new Error('An error occurred while interacting with GPT-3.');
+  }
+}
+
