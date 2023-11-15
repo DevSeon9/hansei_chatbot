c94928448a7194873783714ace6c0e8ea375670c
joseph2462 <joseph2462@hansei.ac.kr>
2023-11-15 09:08:49 +0900

    테스트 코드 프론트 엔드와 연동 성공
    테스트 코드 프론트 엔드와 연동 성공 이후 답변 정확도 향상과 DB연동 예정

--- back_end/framework/Express_test.js
+++ back_end/framework/Express_test.js
@@ -1,43 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // cors 미들웨어 추가
+const cors = require('cors');
 const axios = require('axios');
+
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
-// ChatGPT 연동을 위한 함수
-async function askGPT(question) {
+const OPENAI_API_KEY = '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'; // 수정 필요
+const ENGINE_ID = 'text-davinci-002'; // 또는 원하는 엔진 ID로 변경
+
+async function askGPT(userInput) {
   try {
-    const response = await axios.post('https://api.openai.com/v1/fine-tuning/jobs', {
-      prompt: question,
-      max_tokens: 50,
-      model: 'ft:gpt-3.5-turbo-0613:personal::8GIhhLfw',
-      temperature: 0.1
-    }, {
-      headers: {
-        'Authorization': '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'
+    const response = await axios.post(
+      `https://api.openai.com/v1/engines/${ENGINE_ID}/completions`,
+      {
+        prompt: userInput,
+        max_tokens: 50,
+        temperature: 0.1
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${OPENAI_API_KEY}`
+        }
       }
-    });
+    );
 
     const answer = response.data.choices[0].text;
     return answer;
   } catch (error) {
+    console.error('An error occurred while interacting with GPT-3:', error.response ? error.response.data : error.message);
     throw new Error('An error occurred while interacting with GPT-3.');
   }
 }
 
 app.post('/api/gpt', async (req, res) => {
-  const { question } = req.body;
+  const { userInput } = req.body;
 
   try {
-    const answer = await askGPT(question);
-    res.json({ answer });
+    const answer = await askGPT(userInput);
+    res.json({ message: 'SUCCESS', serverResponse: answer });
   } catch (error) {
-    res.status(500).json({ error: 'ChatGPT 오류' });
+    console.error(error);
+    res.status(500).json({ message: 'FAILURE', error: 'ChatGPT 오류' });
   }
 });
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 const port = 5000;
 app.listen(port, () => {
