7c9ead3ed4643c513cbec88e9fc3563b7fd2457c
joseph2462 <joseph2462@hansei.ac.kr>
2023-11-22 05:44:29 +0900

    연동 선공 질문 제대로 받음
    버그 픽스 및 질문 확인

--- back_end/framework/Express_test.js
+++ back_end/framework/Express_test.js
@@ -8,30 +8,45 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const OPENAI_API_KEY = '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'; // 수정 필요
-const ENGINE_ID = 'text-davinci-002'; // 또는 원하는 엔진 ID로 변경
+// 미세조정 모델의 이름
+const fineTunedModelName = 'ft:gpt-3.5-turbo-0613:personal::8GIhhLfw';
+
+// 기본 GPT-3 모델의 엔드포인트
+const gptEndpoint = 'https://api.openai.com/v1/completions';
+
+// OpenAI API 키
+const apiKey = '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';
+
+// GPT-3와 미세조정에 사용할 모델 선택
+const modelToUse = process.env.USE_FINE_TUNED_MODEL === 'true' ? fineTunedModelName : 'text-davinci-003';
 
 async function askGPT(userInput) {
   try {
     const response = await axios.post(
-      `https://api.openai.com/v1/engines/${ENGINE_ID}/completions`,
+      gptEndpoint,
       {
+        model: modelToUse,
         prompt: userInput,
-        max_tokens: 50,
-        temperature: 0.1
+        max_tokens: 200,
+        temperature: 0.1,
       },
       {
         headers: {
-          'Authorization': `Bearer ${OPENAI_API_KEY}`
-        }
+          'Authorization': `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
       }
     );
 
     const answer = response.data.choices[0].text;
+    
+    // 백엔드에서 서버 콘솔에 로그를 출력
+    console.log('GPT 응답:', answer);
+    console.log('질문:', userInput);
     return answer;
   } catch (error) {
-    console.error('An error occurred while interacting with GPT-3:', error.response ? error.response.data : error.message);
-    throw new Error('An error occurred while interacting with GPT-3.');
+    console.error('GPT-3와 상호 작용 중 오류 발생:', error.response ? error.response.data : error.message);
+    throw new Error('GPT-3와 상호 작용 중 오류가 발생했습니다.');
   }
 }
 
@@ -41,9 +56,10 @@ app.post('/api/gpt', async (req, res) => {
   try {
     const answer = await askGPT(userInput);
     res.json({ message: 'SUCCESS', serverResponse: answer });
+    console.log('응답:', answer);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'FAILURE', error: 'ChatGPT 오류' });
+    res.status(500).json({ message: '실패', serverResponse: null });
   }
 });
 
@@ -58,5 +74,5 @@ app.use(cors(corsOptions));
 
 const port = 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
 });
